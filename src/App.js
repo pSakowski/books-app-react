1606463a364194fa0b7ec48c15cafa116e41c9c8
psakowski <p.sakowski1213@gmail.com>
2022-10-22 22:24:26 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import shortid from 'shortid';
+import BooksList from "./components/BooksList/BooksList";
+import BookForm from "./components/BookForm/BookForm";
 
-function App() {
+const App = () => {
+
+  const [books, setBooks] = useState([
+    { id: 1, title: 'of Mice and men', author: 'John Steinbeck'},
+    { id: 2, title: 'the Witcher', author: 'Andrzej Sapkowski' },
+  ]);
+
+  const removeBook = bookId => {
+    setBooks(books.filter(book => book.id !== bookId))
+  }
+
+  const addBook = newBook => {
+    setBooks([...books, { id: shortid(), title: newBook.title, author: newBook.author }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Books App</h1>
+      <BooksList books={books} removeBook={removeBook}/>
+      <BookForm addBook={addBook}/>
     </div>
   );
 }
